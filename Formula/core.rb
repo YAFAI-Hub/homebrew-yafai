57fd53e22cee49e57dd9e31c7a129d69d54e9231
ssbodapati <susanth.bodapati@gmail.com>
2025-04-07 21:35:45 +0530

    syntax fix

--- Formula/core.rb
+++ Formula/core.rb
@@ -1,16 +1,19 @@
 class Core < Formula
-  desc "Core CLI tool from YAFAI-Hub"
-  homepage "https://github.com/${{ steps.vars.outputs.repo }}"
-  url "${{ steps.vars.outputs.url }}"
-  sha256 "${{ steps.vars.outputs.sha }}"
-  license "MIT"
-  depends_on "go" => :build
+    desc "Core CLI tool from YAFAI-Hub"
+    homepage "https://github.com/YAFAI-Hub/core"
+    url "https://github.com/YAFAI-Hub/core/archive/refs/tags/v0.1.0.tar.gz"
+    sha256 "2a60675df871930d404075d9d1ff0df97926bc511ec47f58459df8f0ff1ebb47"
+    license "MIT"
 
-  def install
-      system "go", "build", *std_go_args(output: bin/"core")
-  end
+    depends_on "go" => :build
 
-  test do
-      assert_match "core version", shell_output("\#{bin}/core --version")
-  end
+    def install
+        system "go", "build", *std_go_args(output: bin/"core")
+    end
+
+    test do
+        assert_match "core version", shell_output("\#{bin}/core --version")
+    end
 end
+
+
