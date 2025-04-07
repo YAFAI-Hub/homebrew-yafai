e8fa13c47443d3d55a477801e45ba6b8cb2dd4a6
ssbodapati <ssbodapati@users.noreply.github.com>
2025-04-07 14:04:58 +0000

    Update core formula to version v0.1.0

--- Formula/core.rb
+++ Formula/core.rb
@@ -1,18 +1,18 @@
   class Core < Formula
-  desc "Core CLI tool from YAFAI-Hub"
-  homepage "https://github.com/YAFAI-Hub/core"
-  url "https://github.com/YAFAI-Hub/core/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "04229c27d355210a6c1133ef01647adc49315262bcb34f881ba5b7c04f32af5b"
-  license "MIT"
+      desc "Core CLI tool from YAFAI-Hub"
+      homepage "https://github.com/YAFAI-Hub/core"
+      url "https://github.com/YAFAI-Hub/core/archive/refs/tags/v0.1.0.tar.gz"
+      sha256 "2a60675df871930d404075d9d1ff0df97926bc511ec47f58459df8f0ff1ebb47"
+      license "MIT"
 
-  depends_on "go" => :build
+      depends_on "go" => :build
 
-  def install
-      system "go", "build", *std_go_args(output: bin/"core")
-  end
+      def install
+          system "go", "build", *std_go_args(output: bin/"core")
+      end
 
-  test do
-      assert_match "core version", shell_output("\#{bin}/core --version")
-  end
+      test do
+          assert_match "core version", shell_output("\#{bin}/core --version")
+      end
   end
   EOF
