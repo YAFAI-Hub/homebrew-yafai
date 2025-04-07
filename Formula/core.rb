f36c209b081249687297cfd3c396243c6de44e90
Sushanth Srivatsa <65614803+ssbodapati@users.noreply.github.com>
2025-04-07 21:43:21 +0530

    V0.1 syntax fix (#5)
    * syntax fix

--- Formula/core.rb
+++ Formula/core.rb
@@ -8,10 +8,10 @@ class Core < Formula
     depends_on "go" => :build
 
     def install
-        system "go", "build", *std_go_args(output: bin/"core")
+        system "go", "build", *std_go_args(output: bin/"yafai-core")
     end
 
     test do
-        assert_match "core version", shell_output("\#{bin}/core --version")
+        assert_match "core version", shell_output("\#{bin}/yafai-core --version")
     end
 end
