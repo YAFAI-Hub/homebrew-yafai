5e1cf47be95b797efebf5d7aba151e8ba729d14c
ssbodapati <susanth.bodapati@gmail.com>
2025-04-09 05:31:17 +0530

    updated formula

--- Formula/core.rb
+++ Formula/core.rb
@@ -1,17 +1,23 @@
 class Core < Formula
     desc "Core CLI tool from YAFAI-Hub"
     homepage "https://github.com/YAFAI-Hub/core"
-    url "https://github.com/YAFAI-Hub/core/archive/refs/tags/v0.0.1.tar.gz"
-    sha256 "0010229d93545fb9b2abbbb76c0f05a029446aed596910e6c5f5fdd90cf36e7c"
-    license "MIT"
-
-    depends_on "go" => :build
-
-    def install
-        system "go", "build", *std_go_args(output: bin/"yafai-core")
+    on_macos do
+      if Hardware::CPU.arm?
+        url "https://github.com/YAFAI-Hub/core/releases/download/v0.0.1/yafai-core-darwin-arm64.tar.gz"
+        sha256 "YOUR_ARM64_MACOS_SHA256"
+      else
+        url "https://github.com/YAFAI-Hub/core/releases/download/v0.0.1/yafai-core-darwin-amd64.tar.gz"
+        sha256 "YOUR_AMD64_MACOS_SHA256"
+      end
     end
-
-    test do
-        assert_match "core version", shell_output("\#{bin}/yafai-core --version")
+    
+    on_linux do
+      if Hardware::CPU.arm?
+        url "https://github.com/YAFAI-Hub/core/releases/download/v0.0.1/yafai-core-linux-arm64.tar.gz"
+        sha256 "YOUR_ARM64_LINUX_SHA256"
+      else
+        url "https://github.com/YAFAI-Hub/core/releases/download/v0.0.1/yafai-core-linux-amd64.tar.gz"
+        sha256 "YOUR_AMD64_LINUX_SHA256"
+      end
     end
 end
