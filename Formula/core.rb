fdf1bc135d083ff9251c6f6bc22cce3aa9c641aa
ssbodapati <susanth.bodapati@gmail.com>
2025-04-09 05:45:55 +0530

    fixed release bin paths

--- Formula/core.rb
+++ Formula/core.rb
@@ -3,20 +3,22 @@ class Core < Formula
     homepage "https://github.com/YAFAI-Hub/core"
     on_macos do
       if Hardware::CPU.arm?
-        url "https://github.com/YAFAI-Hub/core/releases/download/v0.0.1/yafai-core-darwin-arm64.tar.gz"
+        url "https://github.com/YAFAI-Hub/core/releases/download/v0.0.1/yafai-core_Darwin_arm64.tar.gz
+"
         sha256 "YOUR_ARM64_MACOS_SHA256"
       else
-        url "https://github.com/YAFAI-Hub/core/releases/download/v0.0.1/yafai-core-darwin-amd64.tar.gz"
+        url "https://github.com/YAFAI-Hub/core/releases/download/v0.0.1/yafai-core_Darwin_x86_64.tar.gz"
         sha256 "YOUR_AMD64_MACOS_SHA256"
       end
     end
     
     on_linux do
       if Hardware::CPU.arm?
-        url "https://github.com/YAFAI-Hub/core/releases/download/v0.0.1/yafai-core-linux-arm64.tar.gz"
+        url "https://github.com/YAFAI-Hub/core/releases/download/v0.0.1/yafai-core_Linux_arm64.tar.gz
+"
         sha256 "YOUR_ARM64_LINUX_SHA256"
       else
-        url "https://github.com/YAFAI-Hub/core/releases/download/v0.0.1/yafai-core-linux-amd64.tar.gz"
+        url "https://github.com/YAFAI-Hub/core/releases/download/v0.0.1/yafai-core_Linux_amd64.tar.gz"
         sha256 "YOUR_AMD64_LINUX_SHA256"
       end
     end
